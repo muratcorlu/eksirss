dbe4bbddfca3383dd8d16fa8dbfcd25a28b9236c
Murat Çorlu <muratcorlu@gmail.com>
2016-12-14 16:40:24 +0300

    Basliklari cache'leyelim

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import logging
 
 from flask import Flask, render_template, request, make_response
@@ -5,10 +6,13 @@ import requests
 import requests_toolbelt.adapters.appengine
 from lxml import html, etree
 from datetime import datetime
+from google.appengine.api.memcache import Client
 
 app = Flask(__name__)
 
 requests_toolbelt.adapters.appengine.monkeypatch()
+
+cache = Client()
 
 @app.route('/')
 def main():
@@ -18,36 +22,47 @@ def main():
 def feed():
     keyword = request.args.get('t')
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+    cache_key = u'feed-%s' % keyword
 
-    params = {'q': keyword}
+    if cache.get(cache_key) is None:
+        print 'cache yok'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
-    page = requests.get('https://eksisozluk.com/', params=params, headers=headers)
-    tree = html.fromstring(page.content)
+        params = {'q': keyword}
 
-    # get last page
-    last_page_number = tree.xpath('//*[@class="pager"]/@data-pagecount')[0]
-
-    if last_page_number > 1:
-        params = {
-            'p': last_page_number
-        }
-        page = requests.get(page.url, params=params, headers=headers)
-
+        page = requests.get('https://eksisozluk.com/', params=params, headers=headers)
         tree = html.fromstring(page.content)
 
-    meta = {
-        'query': keyword,
-        'title': tree.xpath('//*[@id="title"]/a/span/text()')[0],
-        'url': request.url,
-        'date': ''
-    }
-    entries = [etree.tostring(entry) for entry in tree.xpath('//*[@id="entry-list"]/li/div[1]')][::-1]
-    links = tree.xpath('//*[@class="entry-date permalink"]/@href')[::-1]
-    authors = tree.xpath('//*[@class="entry-author"]/text()')[::-1]
-    dates = [datetime.strptime(date.split(' ~ ')[0], "%d.%m.%Y %H:%M") for date in tree.xpath('//*[@class="entry-date permalink"]/text()')][::-1]
+        # get last page
+        pager = tree.xpath('//*[@class="pager"]/@data-pagecount')
 
-    rss_response = render_template('rss_tpl.xml', entries=entries, links=links, authors=authors, dates=dates, meta=meta)
+        if len(pager) > 0:
+            last_page_number = tree.xpath('//*[@class="pager"]/@data-pagecount')[0]
+
+            if last_page_number > 1:
+                params = {
+                    'p': last_page_number
+                }
+                page = requests.get(page.url, params=params, headers=headers)
+
+                tree = html.fromstring(page.content)
+
+        meta = {
+            'query': keyword,
+            'title': tree.xpath('//*[@id="title"]/a/span/text()')[0],
+            'url': request.url,
+            'date': ''
+        }
+        entries = [etree.tostring(entry) for entry in tree.xpath('//*[@id="entry-list"]/li/div[1]')][::-1]
+        links = tree.xpath('//*[@class="entry-date permalink"]/@href')[::-1]
+        authors = tree.xpath('//*[@class="entry-author"]/text()')[::-1]
+        dates = [datetime.strptime(date.split(' ~ ')[0], "%d.%m.%Y %H:%M") for date in tree.xpath('//*[@class="entry-date permalink"]/text()')][::-1]
+
+        rss_response = render_template('rss_tpl.xml', entries=entries, links=links, authors=authors, dates=dates, meta=meta)
+        cache.set(cache_key, rss_response, time=15*60)
+    else:
+        rss_response = cache.get(cache_key)
+
     response = make_response(rss_response)
     response.headers["Content-Type"] = "application/rss+xml"
 
